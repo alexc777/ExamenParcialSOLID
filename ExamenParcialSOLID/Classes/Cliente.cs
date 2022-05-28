019b18cd28c336363179d7df7cda01d6d2fdbc91
alexc777 <osmancruz187@gmail.com>
2022-05-28 09:53:48 -0600

    creación de clases base, para poder aplicar SOLID

---
+++ ExamenParcialSOLID/Classes/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenParcialSOLID.Classes
+{
+    public class Cliente
+    {
+        public int idCliente { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string identificador { get; set; }
+        public string correo { get; set; }
+        public string pais { get; set; }
+        public string ciudad { get; set; }
+        public double salario { get; set; }
+        public int idRubro { get; set; }
+
+    }
+}
---
+++ ExamenParcialSOLID/Classes/Rubros.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenParcialSOLID.Classes
+{
+    public class Rubros
+    {
+        public int idRubro { get; set; }
+        public string nombre { get; set; }
+        public float porcentaje { get; set; }
+    }
+}
